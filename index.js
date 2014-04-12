026a8c0f001800c7e4304105af14cf1c1f56e933
Richard Walker <digitalsadhu@gmail.com>
2014-04-12 16:56:23 +0200

    Update module to match tests

--- index.js
+++ index.js
@@ -11,8 +11,27 @@ var write = function write(chunk) {
   var lines = data.split('\n')
 
   lines.forEach(function (line) {
+
+    //generic catch all
     if (!line) return
+
+    //skip empty lines
+    if (line.match(/^\s*$/)) return
+
+    //skip lines starting with comments
     if (line.match(/^\s*#.*$/)) return
+
+    //skip lines that start with =
+    if (line.match(/^\s*=.*$/)) return
+
+    //skip lines that dont have an =
+    if (!line.match(/^.*=.*$/)) return
+
+    //trim spaces
+    line = line.replace(/\s*=\s*/, '=')
+    line = line.replace(/^\s*(.*)\s*=\s*(.*)$/, '$1=$2')
+    line = line.replace(/\s$/, '')
+
     self.emit('data', line)
   })
 
@@ -22,5 +41,7 @@ var end = function end () {
   this.emit('end')
 }
 
-module.exports = through(write, end)
+module.exports = function () {
+  return through(write, end)
+}
 
