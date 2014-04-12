7ba7a126a5266f72386adc3b8306a252e693fa52
Richard Walker <digitalsadhu@gmail.com>
2014-04-12 16:55:17 +0200

    Add tests

---
+++ test/env-reader.spec.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var envReader = require('../index.js')
+  , expect    = require('chai').expect
+  , env
+
+describe('env-reader module', function () {
+
+  describe('reading env vars', function () {
+
+    it('should handle string input', function (done) {
+      env = envReader().on('data', function (datum) {
+        expect(datum).to.equal('test=test')
+        done()
+      })
+      env.write('test=test')
+    })
+
+    it('should handle buffer input', function (done) {
+      env = envReader().on('data', function (datum) {
+        expect(datum).to.equal('test=test')
+        done()
+      })
+      env.write(new Buffer('test=test', 'utf8'))
+    })
+
+    it('should skip blank lines', function (done) {
+      env = envReader().on('data', function (datum) {
+        expect(datum).to.equal('test=test')
+        done()
+      })
+      env.write(new Buffer('\n   \ntest=test', 'utf8'))
+    })
+
+    it('should skip comment lines', function (done) {
+      env = envReader().on('data', function (datum) {
+        expect(datum).to.equal('test=test')
+        done()
+      })
+      env.write(new Buffer('\n #asd  \n   \ntest=test', 'utf8'))
+    })
+
+    it('should skip lines with = as the first non space character', function (done) {
+      env = envReader().on('data', function (datum) {
+        expect(datum).to.equal('test=test')
+        done()
+      })
+      env.write(new Buffer('\n #asd  \n  =asd \ntest=test', 'utf8'))
+    })
+
+    it('should skip lines without an = character', function (done) {
+      env = envReader().on('data', function (datum) {
+        expect(datum).to.equal('test=test')
+        done()
+      })
+      env.write(new Buffer('\n #asd  \n  asd \ntest=test', 'utf8'))
+    })
+
+    it('should trim whitespace where possible', function (done) {
+      env = envReader().on('data', function (datum) {
+        expect(datum).to.equal('test=test')
+        done()
+      })
+      env.write(new Buffer('\n test = test \n', 'utf8'))
+    })
+
+  })
+})
